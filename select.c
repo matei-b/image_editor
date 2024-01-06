e84a8b716d31343f6765d9d67f1369c0bd9774ac
Balan Matei <bmatey08@yahoo.com>
2024-01-06 16:55:46 +0200

    Error output SELECT fixed

--- select.c
+++ select.c
@@ -6,7 +6,7 @@
 
 void select_img(struct image *img, int allocated)
 {
-	int x1, y1, x2, y2;
+	int x1 = -1, y1 = -1, x2 = -1, y2 = -1;
 	char line[MAX_LEN];
 	fgets(line, sizeof(line), stdin);
 	if (allocated == 0) {
@@ -18,10 +18,18 @@ void select_img(struct image *img, int allocated)
 			img->sel.x2 = img->x2;
 			img->sel.y2 = img->y2;
 			printf("Selected ALL\n");
-
 		} else {
 			sscanf(line, " %d %d %d %d\n", &x1, &y1, &x2, &y2);
-			if (x1 > img->x2 || x2 > img->x2 || y1 > img->y2 || y2 > img->y2) {
+			int ouchie = 0;
+			if (x1 == -1)
+				ouchie = 1;
+			if (x2 == -1)
+				ouchie = 1;
+			if (y1 == -1)
+				ouchie = 1;	
+			if (y2 == -1)
+				ouchie = 1;	
+			if (x1 > img->x2 || x2 > img->x2 || y1 > img->y2 || y2 > img->y2 || ouchie == 1) {
 				printf("Invalid set of coordinates\n");
 			} else {
 				if (x1 < 0 || y1 < 0 || x2 == x1 || y2 == y1) {
