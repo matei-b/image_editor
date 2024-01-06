d94db4220f1fe3b7c3c50c0cb6be2d6788e1e0e6
Balan Matei <bmatey08@yahoo.com>
2024-01-06 17:12:26 +0200

    Error output SELECT fixed

--- select.c
+++ select.c
@@ -6,7 +6,8 @@
 
 void select_img(struct image *img, int allocated)
 {
-	int x1 = -1, y1 = -1, x2 = -1, y2 = -1;
+	int x1 = NUMBER_NO_ONE_WOULD_THINK_OF, y1 = NUMBER_NO_ONE_WOULD_THINK_OF;
+	int x2 = NUMBER_NO_ONE_WOULD_THINK_OF, y2 = NUMBER_NO_ONE_WOULD_THINK_OF;
 	char line[MAX_LEN];
 	fgets(line, sizeof(line), stdin);
 	if (allocated == 0) {
@@ -21,13 +22,13 @@ void select_img(struct image *img, int allocated)
 		} else {
 			sscanf(line, " %d %d %d %d\n", &x1, &y1, &x2, &y2);
 			int ouchie = 0;
-			if (x1 == -1)
+			if (x1 == NUMBER_NO_ONE_WOULD_THINK_OF)
 				ouchie = 1;
-			if (x2 == -1)
+			if (x2 == NUMBER_NO_ONE_WOULD_THINK_OF)
 				ouchie = 1;
-			if (y1 == -1)
+			if (y1 == NUMBER_NO_ONE_WOULD_THINK_OF)
 				ouchie = 1;	
-			if (y2 == -1)
+			if (y2 == NUMBER_NO_ONE_WOULD_THINK_OF)
 				ouchie = 1;	
 			if (ouchie == 1) {
 				printf("Invalid command\n");
--- select.h
+++ select.h
@@ -1,6 +1,8 @@
 #ifndef SELECT_H_
 #define SELECT_H_
 
+#define NUMBER_NO_ONE_WOULD_THINK_OF 19283120
+
 void select_img(struct image *img, int allocated);
 
 #endif
